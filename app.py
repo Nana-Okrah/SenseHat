9b69efee05933c0ff6023940d463214016cfd61c
Nana Okrah <nanaokrah5@gmail.com>
2021-03-20 18:58:48 -0400

    Finished project

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from sense_hat import SenseHat
 from flask import Flask, render_template, request, current_app as app
+from sense_hat import SenseHat
 import os
 import requests
+import datetime
+sense =SenseHat()
+#color = text_colour[255,165,0] #orange
 
-
+time = datetime.datetime.now()
 app = Flask(__name__)
 
 @app.route('/message')
 def index():
 	return render_template('message.html')
 	
-@app.route('/show_messages',methods =["POST","GET"])
-def login():
-	if request.method == 'POST':
-		user = request.form['name', 'message']
-		return redirect(url_for('/show_message', name =user))
-
+@app.route('/success', methods=['GET','POST'])
+def success():
+   message = request.form['message']
+   name = request.form['name']
+   sense.show_message(name +":" + message)
+   return render_template('sended_messages.html', name = name, message = message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
