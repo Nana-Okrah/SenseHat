a4ab28fa5ca44c367a13876503bb48e3129a1179
Nana Okrah <nanaokrah5@gmail.com>
2021-04-17 10:46:21 -0400

    added a DB

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from sense_hat import SenseHat
 import os
 import requests
 import datetime
+import sqlite3
+import sys
+
 sense =SenseHat()
 #color = text_colour[255,165,0] #orange
 
@@ -18,7 +21,18 @@ def success():
    message = request.form['message']
    name = request.form['name']
    sense.show_message(name +":" + message)
+   conn = sqlite3.connect('./static/data/messages.db')
+   curs = conn.cursor()
+   curs.execute("INSERT INTO messages VALUES((?),(?))",(name, message))
+   conn.commit()
+   
+   conn.close()
    return render_template('sended_messages.html', name = name, message = message)
+   
+#@app.route('/l')
+#def info():
+    #for message in sended_message.html:
+	#return send_message.html
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
